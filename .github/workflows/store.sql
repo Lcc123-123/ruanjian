0e6fb2880e2061c24e6a3a7d905d3539a82e7bc3
梁琛琛 <17075970+liang-chenchenchen@user.noreply.gitee.com>
2026-06-11 10:26:24 +0800

    新增数据库配置，修复单元测试连接MySQL失败

---
+++ .github/workflows/store.sql
@@ -0,0 +1,6 @@
+spring:
+  datasource:
+    url: jdbc:mysql://127.0.0.1:3306/test_db?serverTimezone=UTC&useSSL=false
+    username: root
+    password: root
+    driver-class-name: com.mysql.cj.jdbc.Driver
