fe8433fb3a385e5786b7bd14c2742d6d0b1ee680
梁琛琛 <17075970+liang-chenchenchen@user.noreply.gitee.com>
2026-06-11 10:31:12 +0800

    新增store.sql文件，修复找不到数据库脚本问题

--- .github/workflows/store.sql
+++ .github/workflows/store.sql
@@ -2,5 +2,5 @@ spring:
   datasource:
     url: jdbc:mysql://127.0.0.1:3306/test_db?serverTimezone=UTC&useSSL=false
     username: root
-    password: root
+    password: 123456
     driver-class-name: com.mysql.cj.jdbc.Driver
